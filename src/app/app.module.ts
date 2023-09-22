f4f449419b56d9c9b41edb47b0d05a8d92cbb36e
Giacomo Colle <giacomo.colle@buonarroti.tn.it>
2023-09-22 16:23:44 +0200

    fix: added ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { Pipe, PipeTransform } from '@angular/core'; 
 import { CourseFilterNamePipe,CourseFilterClassePipe,CourseFilterAulaPipe, ArraySortPipe } from './coursefilters.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { CourseFilterNamePipe,CourseFilterClassePipe,CourseFilterAulaPipe, Array
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
